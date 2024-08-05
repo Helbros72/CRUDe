12c9c70ec32215a39905c8371fbd2d30aad08e61
Vitaly L <111348000+vetrof@users.noreply.github.com>
2024-08-05 20:32:53 +0500

    Update serializers.py

--- app_products/serializers.py
+++ app_products/serializers.py
@@ -3,7 +3,7 @@ from rest_framework import serializers
 from app_products.models import Product
 
 
-class ProductSerializer(serializers.Serializer):
+class ProductSerializer(serializers.ModelSerializer):
     class Meta:
         model = Product
         fields = ["name", "price"]
